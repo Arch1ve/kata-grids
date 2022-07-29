f5ec12d3338b053910e6e992a173033d80434848
Arc1ve <afanasiy.m2002@gmail.com>
2022-07-29 12:06:54 +0300

    Made services section for all resolutions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-"use strict"
+const brandsOpener = document.querySelector(".brands__opener-button");
+const brandsContent = document.querySelector(".brands__wrapper");
+let openState = false;
+
+brandsOpener.addEventListener('click', function() {
+  if (openState) {
+    brandsContent.classList.add('brands-hidden');
+    brandsOpener.classList.remove('close-opener-bg');
+    brandsOpener.textContent = "Показать все"
+    openState = false;
+  } else {
+    brandsContent.classList.remove('brands-hidden');
+    brandsOpener.classList.add('close-opener-bg');
+    brandsOpener.textContent = "Скрыть"
+    openState = true;
+  }
+});
+
+
